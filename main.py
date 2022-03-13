ddfed32bfa70670c84587153725e481c51b6763e
imram3 <imranraad@gmail.com>
2022-03-13 03:57:42 -0400

    filters added

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ from pathlib import Path
 from PIL import Image
 import numpy as np
 
-from utils import rgb_to_gray, salt_pepper_noise, gaussian_noise, equalized_histogram, mean_square_error
+from utils import rgb_to_gray, salt_pepper_noise, gaussian_noise, equalized_histogram, mean_square_error, linear_filter, \
+    median_filter
 
 
 def test_utils(image_path):
     img = Image.open(image_path)
     # Output Images
-    img.show()
+    # img.show()
     # print(img.format)
     # print(img.mode)
 
@@ -26,15 +27,15 @@ def test_utils(image_path):
     # salt n pepper noise
     print("Salt and Pepper Noise...")
     noised_image = salt_pepper_noise(gray_image, 0.1)
-    noised_image1 = Image.fromarray(noised_image)
-    noised_image1.show()
+    # noised_image1 = Image.fromarray(noised_image)
+    # noised_image1.show()
     print("Done.")
 
     # gaussian noise
     print("Gaussian Noise...")
     noised_image_2 = gaussian_noise(gray_image, 10)
-    noised_image_21 = Image.fromarray(noised_image_2)
-    noised_image_21.show()
+    # noised_image_21 = Image.fromarray(noised_image_2)
+    # noised_image_21.show()
     print("Done.")
 
     # # histogram
@@ -46,8 +47,8 @@ def test_utils(image_path):
     print("Equalization Histogram...")
     histogram_org, histogram_eq, img_eq = equalized_histogram(gray_image)
     # print(img_eq.shape)
-    img_eq1 = Image.fromarray(img_eq)
-    img_eq1.show()
+    # img_eq1 = Image.fromarray(img_eq)
+    # img_eq1.show()
     # print(histogram_org)
     # print(histogram_eq)
     print("Done.")
@@ -56,6 +57,14 @@ def test_utils(image_path):
     msqe = mean_square_error(gray_image, img_eq)
     print(msqe)
     print("Done.")
+
+    linear_filtered_img = linear_filter(gray_image, np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]]), 9)
+    linear_filtered_img1 = Image.fromarray(linear_filtered_img)
+    linear_filtered_img1.show()
+
+    median_filtered_img = median_filter(gray_image, np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+    median_filtered_img1 = Image.fromarray(median_filtered_img)
+    median_filtered_img1.show()
 
 
 def operations(image_path):
@@ -67,6 +76,8 @@ def operations(image_path):
     noised_image_2 = gaussian_noise(gray_image, 10)
     histogram_org, histogram_eq, img_eq = equalized_histogram(gray_image)
     msqe = mean_square_error(gray_image, img_eq)
+    linear_filtered_img = linear_filter(gray_image, np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]]), 9)
+    median_filtered_img = median_filter(gray_image, np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
     print("Ended..")
     t1 = time.time()
     print("Time Taken", t1 - t0, image_path)
@@ -75,7 +86,7 @@ def operations(image_path):
 if __name__ == "__main__":
     # test_utils('data/Cancerous cell smears/cyl01.BMP')
     base_path = Path('data/Cancerous cell smears')
-    files = list(base_path.glob("*.BMP"))
+    files = list(base_path.glob("inter*.BMP"))
     print(len(files))
     proc = []
     counter = 0
--- utils.py
+++ utils.py
@@ -26,8 +26,16 @@ def histogram(img):
     return hist
 
 
-def gaussian_noise(img_array, sigma):
-    copy_image = img_array.copy()
+def calc_avg_histogram(hist_list):
+    hist_arr = np.array(hist_list)
+    hist = np.mean(hist_arr, axis=0)
+    hist = np.rint(hist)
+    hist = hist.astype(int)
+    return hist
+
+
+def gaussian_noise(img, sigma):
+    copy_image = img.copy()
 
     mean = 0.0
     noise = np.random.normal(mean, sigma, copy_image.size)
@@ -73,3 +81,42 @@ def equalized_histogram(img):
 def mean_square_error(original_img, quantized_img):
     mse = (np.square(original_img - quantized_img)).mean()
     return mse
+
+
+def linear_filter(img, filter, scale):
+    copy_image = img.copy()
+
+    x, y = filter.shape
+    m, n = copy_image.shape
+    m1 = m - x + 1
+    n1 = n - y + 1
+    out = np.zeros((m1, n1))
+
+    for i in range(m1):
+        for j in range(n1):
+            neighborhood = copy_image[i: i + x, j: j + y]
+            result = np.sum(filter * neighborhood)
+            scaled_result = result / scale
+            out[i, j] = scaled_result
+    out = np.rint(out)
+    return out.astype(np.uint8)
+
+
+def median_filter(img, weights):
+    copy_image = img.copy()
+
+    x, y = weights.shape
+    m, n = copy_image.shape
+    m1 = m - x + 1
+    n1 = n - y + 1
+    out = np.zeros((m1, n1))
+
+    for i, j in np.ndindex(out.shape):
+        neighborhood = copy_image[i: i + x, j: j + y]
+        pixel_list = np.array([])
+        for k, l in np.ndindex(neighborhood.shape):
+            pixel_list = np.append(pixel_list, [neighborhood[k, l]] * weights[k, l])
+        result = np.median(pixel_list)
+        out[i, j] = result
+    out = np.rint(out)
+    return out.astype(np.uint8)
