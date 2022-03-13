73c85226ad6e3c4d13e89bb6457a2ef374c32f95
imram3 <imranraad@gmail.com>
2022-03-13 14:39:14 -0400

    file save added

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # importing PIL
+import argparse
+import array
 import time
 from pathlib import Path
 
 import numpy as np
 from PIL import Image
 from PIL.ImageFile import ImageFile
-
-import tqdm_batch
+from batch_process import batch_process
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
@@ -75,25 +76,56 @@ def test_utils(image_path):
   print("Done.")
 
 
-def operations(image_path):
+def save_image(name, data):
+  img = Image.fromarray(data)
+  if img.mode != 'RGB':
+    img = img.convert('RGB')
+  img.save(name)
+
+
+def operations(image_path, args):
   t0 = time.time()
+  file_name = image_path.stem
   img = Image.open(image_path)
-  gray_image = rgb_to_gray(np.array(img), 'G')
-  noised_image = salt_pepper_noise(gray_image, 0.1)
-  noised_image_2 = gaussian_noise(gray_image, 10)
+  gray_image = rgb_to_gray(np.array(img), args.color_channel)
+  noised_image = salt_pepper_noise(gray_image, args.salt_pepper_noise_strength)
+  save_image(args.output_path + '/' + file_name + '_salt_n_pepper.' + args.image_type, noised_image)
+  noised_image_2 = gaussian_noise(gray_image, args.gaussian_strength)
+  save_image(args.output_path + '/' + file_name + '_gaussian.' + args.image_type, noised_image_2)
   histogram_org, histogram_eq, img_eq = equalized_histogram(gray_image)
+  save_image(args.output_path + '/' + file_name + '_histogram.' + args.image_type, histogram_org)
+  save_image(args.output_path + '/' + file_name + '_equalized_histogram.' + args.image_type, histogram_eq)
+  save_image(args.output_path + '/' + file_name + '_equalized.' + args.image_type, img_eq)
   msqe = mean_square_error(gray_image, img_eq)
-  linear_filtered_img = linear_filter(noised_image, np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]]))
-  median_filtered_img = median_filter(noised_image, 3)
+  weight = np.array(args.linear_filter_weights.split())
+  weight = weight.astype(int)
+  weight = weight.reshape(args.linear_filter_mask, args.linear_filter_mask)
+  # linear_filtered_img = linear_filter(noised_image, weight)
+  # median_filtered_img = median_filter(noised_image, 3)
   t1 = time.time()
+  print(file_name, "Finished. MSQE", msqe, 'Average operation time', t1 - t0)
 
 
-def batch_process_function(image_path):
-  return operations(image_path)
+def batch_process_function(image_path, args):
+  return operations(image_path, args)
 
 
 if __name__ == "__main__":
   # test_utils('data/Cancerous cell smears/cyl01.BMP')
-  base_path = Path('data/Cancerous cell smears')
-  files = list(base_path.glob("*.BMP"))
-  tqdm_batch.batch_process(files, batch_process_function, n_workers=5, sep_progress=True)
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--base_path', type=str, required=True)
+  parser.add_argument('--output_path', type=str, required=True)
+  parser.add_argument('--image_type', type=str, required=True)
+  parser.add_argument('--batch_size', type=int, required=True, default=10)
+  parser.add_argument('--color_channel', type=str, required=False, choices=['R', 'G', 'B'], default=None)
+  parser.add_argument('--linear_filter_weights', type=str, required=False, default='1 1 1 1 1 1 1 1 1')
+  parser.add_argument('--linear_filter_mask', type=int, required=False, default=3)
+  parser.add_argument('--salt_pepper_noise_strength', type=float, required=False, default=0.1)
+  parser.add_argument('--gaussian_strength', type=int, required=False, default=10)
+
+  args = parser.parse_args()
+
+  base_path = Path(args.base_path)
+  files = list(base_path.glob("*." + args.image_type))
+  batch_size = args.batch_size
+  batch_process(files, batch_process_function, batch_size=batch_size, args=args)
---
+++ run.sh
@@ -0,0 +1,9 @@
+python main.py --base_path 'data/Cancerous cell smears' \
+  --output_path 'output' \
+  --image_type 'BMP' \
+  --batch_size 10 \
+  --color_channel 'R' \
+  --linear_filter_weights '1 1 1 1 1 1 1 1 1' \
+  --linear_filter_mask 3 \
+  --salt_pepper_noise_strength 0.1 \
+  --gaussian_strength 1
