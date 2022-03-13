4e2919f96f039ec3f9cd246d2dd26a40102ec26d
imram3 <imranraad@gmail.com>
2022-03-13 00:31:26 -0500

    eq hist added

--- main.py
+++ main.py
@@ -3,58 +3,47 @@ import random
 
 from PIL import Image, ImageFilter
 import numpy as np
-from utils import rgb_to_gray, salt_pepper_noise
-
-
-# function to obtain histogram of an image
-def hist_plot(img):
-    m, n = img.shape
-    count = []
-    r = []
-    for k in range(0, 256):
-        r.append(k)
-        count1 = 0
-        for i in range(m):
-            for j in range(n):
-                if img[i, j] == k:
-                    count1 += 1
-        count.append(count1)
-    return (r, count)
-
-
-
-
-def gaussian_noise(img_array, sigma):
-    mean = 0.0
-    noise = np.random.normal(mean, sigma, img_array.size)
-    shaped_noise = noise.reshape(img_array.shape)
-    gauss = img_array + shaped_noise
-    return gauss
-
+from utils import rgb_to_gray, salt_pepper_noise, histogram, gaussian_noise, equalized_histogram
 
 if __name__ == "__main__":
     img = Image.open('data/Cancerous cell smears/cyl01.BMP')
     # Output Images
-    img.show()
-    print(img.format)
-    print(img.mode)
+    # img.show()
+    # print(img.format)
+    # print(img.mode)
 
     # convert to gray image
+    print("Gray Image...")
     gray_image = rgb_to_gray(np.array(img))
-    gray_image1 = Image.fromarray(gray_image)
-    gray_image1.show()
+    # gray_image1 = Image.fromarray(gray_image)
+    # gray_image1.show()
+    print("Done.")
 
     # salt n pepper noise
+    print("Salt and Pepper Noise...")
     noised_image = salt_pepper_noise(gray_image, 0.1)
-    noised_image1 = Image.fromarray(noised_image)
-    noised_image1.show()
+    # noised_image1 = Image.fromarray(noised_image)
+    # noised_image1.show()
+    print("Done.")
 
     # gaussian noise
+    print("Gaussian Noise...")
     noised_image_2 = gaussian_noise(gray_image, 0.1)
-    noised_image_21 = Image.fromarray(noised_image_2)
-    noised_image_21.show()
+    # noised_image_21 = Image.fromarray(noised_image_2)
+    # noised_image_21.show()
+    print("Done.")
 
-    # histogram
-    r1, count = hist_plot(gray_image)
-    print(r1)
-    print(count)
+    # # histogram
+    # print("Histogram...")
+    # histogram = histogram(gray_image)
+    # print(histogram)
+    # print("Done.")
+
+    print("Equalization Histogram...")
+    histogram_org, histogram_eq, img_eq = equalized_histogram(gray_image)
+    # print(img_eq.shape)
+    # img_eq1 = Image.fromarray(img_eq)
+    # img_eq1.show()
+    # print(histogram_org)
+    # print(histogram_eq)
+    print("Done.")
--- utils.py
+++ utils.py
@@ -2,10 +2,11 @@ import numpy as np
 
 
 def rgb_to_gray(img):
-    grayImage = np.zeros(img.shape)
-    R = np.array(img[:, :, 0])
-    G = np.array(img[:, :, 1])
-    B = np.array(img[:, :, 2])
+    copy_image = img.copy()
+
+    R = np.array(copy_image[:, :, 0])
+    G = np.array(copy_image[:, :, 1])
+    B = np.array(copy_image[:, :, 2])
 
     R = (R * .299)
     G = (G * .587)
@@ -13,28 +14,60 @@ def rgb_to_gray(img):
 
     Avg = (R + G + B)
     return Avg
-    # grayImage = img.copy()
-    #
-    # for i in range(3):
-    #     grayImage[:, :, i] = Avg
-    #
-    # return grayImage
+
+
+def histogram(img):
+    m, n = img.shape
+    count = []
+    for k in range(0, 256):
+        count1 = 0
+        for i in range(m):
+            for j in range(n):
+                if img[i, j] == k:
+                    count1 += 1
+        count.append(count1)
+    return np.array(count)
+
+
+def gaussian_noise(img_array, sigma):
+    copy_image = img_array.copy()
+
+    mean = 0.0
+    noise = np.random.normal(mean, sigma, copy_image.size)
+    shaped_noise = noise.reshape(copy_image.shape)
+    gauss = copy_image + shaped_noise
+    return gauss
 
 
 def salt_pepper_noise(img, strength):
-    out = np.copy(img)
-    m, n = img.shape
+    copy_image = img.copy()
 
-    num_salt = np.ceil(strength * img.size * 0.5)
+    out = np.copy(copy_image)
+    m, n = copy_image.shape
+
+    num_salt = np.ceil(strength * copy_image.size * 0.5)
     for i in range(int(num_salt)):
         x = np.random.randint(0, m - 1)
         y = np.random.randint(0, n - 1)
         out[x][y] = 0
 
-    num_pepper = np.ceil(strength * img.size * 0.5)
+    num_pepper = np.ceil(strength * copy_image.size * 0.5)
     for i in range(int(num_pepper)):
         x = np.random.randint(0, m - 1)
         y = np.random.randint(0, n - 1)
         out[x][y] = 0
 
     return out
+
+
+def equalized_histogram(img):
+    copy_image = img.copy()
+
+    img_hist = histogram(copy_image)
+    cum_sum = np.cumsum(img_hist)
+    cum_sum = (cum_sum - cum_sum.min()) * 255 / (cum_sum.max() - cum_sum.min())
+    cum_sum = cum_sum.astype(np.uint8)
+    equalized = cum_sum[copy_image.flatten().astype(np.uint8)]
+
+    img_new = np.reshape(equalized, copy_image.shape)
+    return img_hist, histogram(img_new), img_new
