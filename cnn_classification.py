1f44afa433ad5c7bbdeb47aeb1f99269fee5312f
imranraad07 <imranraad@gmail.com>
2022-05-10 05:14:59 -0400

    added cnn

--- cnn_classification.py
+++ cnn_classification.py
@@ -1,14 +1,18 @@
+import random
 from builtins import len
 
 import torch
 import torchvision.transforms as transforms
 import torch.nn.init
 from PIL import Image
+import torch.nn.functional as F
+from sklearn.metrics import roc_curve
+import matplotlib.pyplot as plt
 
 # parameters
 from sklearn import metrics
 
-learning_rate = 0.01
+learning_rate = 0.001
 training_epochs = 30
 batch_size = 50
 
@@ -16,10 +20,23 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 import os
 
-train_dir = 'train'
-test_dir = 'test'
-train_files = os.listdir(train_dir)
-test_files = os.listdir(test_dir)
+data_dir = 'data/Cancerous cell smears'
+data_files = os.listdir(data_dir)
+
+
+def prepare_10fold(data_files):
+    eind = 0
+    random.shuffle(data_files)
+    fold_size = int(len(data_files) / 10)
+    for fold in range(0, 10):
+        sind = eind
+        eind = sind + fold_size
+        train_pair = data_files[0:sind] + data_files[eind:len(data_files)]
+        test_pair = data_files[sind:eind]
+        yield (train_pair, test_pair)
+
+
+data_10fold = prepare_10fold(data_files)
 
 
 class CellsDataset(torch.utils.data.Dataset):
@@ -46,28 +63,40 @@ class CellsDataset(torch.utils.data.Dataset):
             return img.astype('float32'), self.file_list[idx]
 
 
+# (568, 768)
+
+data_transform = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.ToTensor()
+])
+
+input_size = 256
+output_size = 7
+hidden_size = [784, 512, 256, 64]
+
+
 class CNN(torch.nn.Module):
 
     def __init__(self):
         super(CNN, self).__init__()
         self.layer1 = torch.nn.Sequential(
-            torch.nn.Conv2d(3, 784, kernel_size=3, stride=1, padding=1),
+            torch.nn.Conv2d(3, hidden_size[0], kernel_size=3, stride=1, padding=1),
             torch.nn.ReLU(),
             torch.nn.MaxPool2d(kernel_size=2, stride=2))
         self.layer2 = torch.nn.Sequential(
-            torch.nn.Conv2d(784, 512, kernel_size=3, stride=1, padding=1),
+            torch.nn.Conv2d(hidden_size[0], hidden_size[1], kernel_size=3, stride=1, padding=1),
             torch.nn.ReLU(),
             torch.nn.MaxPool2d(kernel_size=2, stride=2))
         self.layer3 = torch.nn.Sequential(
-            torch.nn.Conv2d(512, 256, kernel_size=3, stride=1, padding=1),
+            torch.nn.Conv2d(hidden_size[1], hidden_size[2], kernel_size=3, stride=1, padding=1),
             torch.nn.ReLU(),
             torch.nn.MaxPool2d(kernel_size=2, stride=2))
         self.layer4 = torch.nn.Sequential(
-            torch.nn.Conv2d(256, 64, kernel_size=3, stride=1, padding=1),
+            torch.nn.Conv2d(hidden_size[2], hidden_size[3], kernel_size=3, stride=1, padding=1),
             torch.nn.ReLU(),
             torch.nn.MaxPool2d(kernel_size=2, stride=2))
-        self.fc = torch.nn.Linear(16384, 32, bias=True)
-        self.fc2 = torch.nn.Linear(32, 7, bias=True)
+        self.fc = torch.nn.Linear(input_size * hidden_size[3], 32, bias=True)
+        self.fc2 = torch.nn.Linear(32, output_size, bias=True)
         torch.nn.init.xavier_uniform_(self.fc.weight)
 
     def forward(self, x):
@@ -81,120 +110,137 @@ class CNN(torch.nn.Module):
         return out
 
 
-# (568, 768)
+fold_results = []
+precision = []
+recall = []
+f1 = []
+idx = 0
 
-data_transform = transforms.Compose([
-    transforms.Resize((256, 256)),
-    transforms.ToTensor()
-])
+for item in data_10fold:
+    # print(len(item[0]), len(item[1]))
+    idx = idx + 1
 
-cyl_files = [tf for tf in train_files if 'cyl' in tf]
-inter_files = [tf for tf in train_files if 'inter' in tf]
-mod_files = [tf for tf in train_files if 'mod' in tf]
-let_files = [tf for tf in train_files if 'let' in tf]
-super_files = [tf for tf in train_files if 'super' in tf]
-para_files = [tf for tf in train_files if 'para' in tf]
-svar_files = [tf for tf in train_files if 'svar' in tf]
+    train_files = item[0]
+    test_files = item[1]
 
-cyls = CellsDataset(cyl_files, 0, train_dir, transform=data_transform)
-inters = CellsDataset(inter_files, 1, train_dir, transform=data_transform)
-mods = CellsDataset(mod_files, 2, train_dir, transform=data_transform)
-lets = CellsDataset(let_files, 3, train_dir, transform=data_transform)
-supers = CellsDataset(super_files, 4, train_dir, transform=data_transform)
-paras = CellsDataset(para_files, 5, train_dir, transform=data_transform)
-svars = CellsDataset(svar_files, 6, train_dir, transform=data_transform)
+    cyl_files = [tf for tf in train_files if 'cyl' in tf]
+    inter_files = [tf for tf in train_files if 'inter' in tf]
+    mod_files = [tf for tf in train_files if 'mod' in tf]
+    let_files = [tf for tf in train_files if 'let' in tf]
+    super_files = [tf for tf in train_files if 'super' in tf]
+    para_files = [tf for tf in train_files if 'para' in tf]
+    svar_files = [tf for tf in train_files if 'svar' in tf]
 
-cells = torch.utils.data.ConcatDataset([cyls, inters, mods, lets, supers, paras, svars])
+    cyls = CellsDataset(cyl_files, 0, data_dir, transform=data_transform)
+    inters = CellsDataset(inter_files, 1, data_dir, transform=data_transform)
+    mods = CellsDataset(mod_files, 2, data_dir, transform=data_transform)
+    lets = CellsDataset(let_files, 3, data_dir, transform=data_transform)
+    supers = CellsDataset(super_files, 4, data_dir, transform=data_transform)
+    paras = CellsDataset(para_files, 5, data_dir, transform=data_transform)
+    svars = CellsDataset(svar_files, 6, data_dir, transform=data_transform)
 
-# dataset loader
-data_loader = torch.utils.data.DataLoader(dataset=cells, batch_size=batch_size, shuffle=True, drop_last=True)
+    cells = torch.utils.data.ConcatDataset([cyls, inters, mods, lets, supers, paras, svars])
+    data_loader = torch.utils.data.DataLoader(dataset=cells, batch_size=batch_size, shuffle=True, drop_last=True)
+    model = CNN().to(device)
 
-# instantiate CNN model
-model = CNN().to(device)
+    # print(model)
 
+    # define cost/loss & optimizer
+    criterion = torch.nn.CrossEntropyLoss().to(device)  # Softmax is internally computed.
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
+    # train my model
+    total_batch = len(data_loader)
+    # print('Learning started. It takes sometime.')
+    for epoch in range(training_epochs):
+        avg_cost = 0
 
-# define cost/loss & optimizer
-criterion = torch.nn.CrossEntropyLoss().to(device)  # Softmax is internally computed.
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+        for X, Y in data_loader:
+            X = X.to(device)
+            Y = Y.to(device)
 
-# train my model
-total_batch = len(data_loader)
-print('Learning started. It takes sometime.')
-for epoch in range(training_epochs):
-    avg_cost = 0
+            optimizer.zero_grad()
+            hypothesis = model(X)
+            cost = criterion(hypothesis, Y)
+            cost.backward()
+            optimizer.step()
+
+            avg_cost += cost / total_batch
+
+        print('[Epoch: {:>4}] cost = {:>.9}'.format(epoch + 1, avg_cost))
+
+    # print('Learning Finished!')
+
+    cyl_files = [tf for tf in test_files if 'cyl' in tf]
+    inter_files = [tf for tf in test_files if 'inter' in tf]
+    mod_files = [tf for tf in test_files if 'mod' in tf]
+    let_files = [tf for tf in test_files if 'let' in tf]
+    super_files = [tf for tf in test_files if 'super' in tf]
+    para_files = [tf for tf in test_files if 'para' in tf]
+    svar_files = [tf for tf in test_files if 'svar' in tf]
+
+    cyls = CellsDataset(cyl_files, 0, data_dir, transform=data_transform)
+    inters = CellsDataset(inter_files, 1, data_dir, transform=data_transform)
+    mods = CellsDataset(mod_files, 2, data_dir, transform=data_transform)
+    lets = CellsDataset(let_files, 3, data_dir, transform=data_transform)
+    supers = CellsDataset(super_files, 4, data_dir, transform=data_transform)
+    paras = CellsDataset(para_files, 5, data_dir, transform=data_transform)
+    svars = CellsDataset(svar_files, 6, data_dir, transform=data_transform)
+
+    cells_test = torch.utils.data.ConcatDataset([cyls, inters, mods, lets, supers, paras, svars])
+
+    # dataset loader
+    data_loader = torch.utils.data.DataLoader(dataset=cells_test, batch_size=batch_size, shuffle=True, drop_last=True)
+
+    test_y = []
+    predicted = []
+
+    import numpy as np
 
     for X, Y in data_loader:
         X = X.to(device)
         Y = Y.to(device)
-
+        # print(len(X), len(Y))
         optimizer.zero_grad()
         hypothesis = model(X)
         cost = criterion(hypothesis, Y)
-        cost.backward()
-        optimizer.step()
 
-        avg_cost += cost / total_batch
+        y_true = Y.cpu().detach().numpy()
+        _, results = torch.max(hypothesis.data, 1)
+        print(results)
 
-    print('[Epoch: {:>4}] cost = {:>.9}'.format(epoch + 1, avg_cost))
+        test_y.extend(y_true)
+        predicted.extend(results.cpu().detach())
 
-print('Learning Finished!')
+    test_y = np.array(test_y)
+    predicted = np.array(predicted)
 
-cyl_files = [tf for tf in test_files if 'cyl' in tf]
-inter_files = [tf for tf in test_files if 'inter' in tf]
-mod_files = [tf for tf in test_files if 'mod' in tf]
-let_files = [tf for tf in test_files if 'let' in tf]
-super_files = [tf for tf in test_files if 'super' in tf]
-para_files = [tf for tf in test_files if 'para' in tf]
-svar_files = [tf for tf in test_files if 'svar' in tf]
+    out_res = ''
+    for u in range(7):
+        fpr, tpr, thresholds = metrics.roc_curve(test_y, predicted, pos_label=u)
+        out_res = "auc at " + str(u) + " " + str(metrics.auc(fpr, tpr))
+        print(out_res)
 
-cyls = CellsDataset(cyl_files, 0, test_dir, transform=data_transform)
-inters = CellsDataset(inter_files, 1, test_dir, transform=data_transform)
-mods = CellsDataset(mod_files, 2, test_dir, transform=data_transform)
-lets = CellsDataset(let_files, 3, test_dir, transform=data_transform)
-supers = CellsDataset(super_files, 4, test_dir, transform=data_transform)
-paras = CellsDataset(para_files, 5, test_dir, transform=data_transform)
-svars = CellsDataset(svar_files, 6, test_dir, transform=data_transform)
+    f = open("auc_fold.txt", "a")
+    f.write(out_res)
+    f.close()
 
-cells_test = torch.utils.data.ConcatDataset([cyls, inters, mods, lets, supers, paras, svars])
+    # print("Precision:", round(metrics.precision_score(test_y, predicted, average="micro"), 3),
+    #       "Recall:", round(metrics.recall_score(test_y, predicted, average="micro"), 3),
+    #       "F1-score:", round(metrics.f1_score(test_y, predicted, average="micro"), 3))
+    res = str(idx) + " & " + str(round(metrics.precision_score(test_y, predicted, average="micro"), 2)) + " & " + \
+          str(round(metrics.precision_score(test_y, predicted, average="micro"), 2)) + " & " + \
+          str(round(metrics.precision_score(test_y, predicted, average="micro"), 2))
+    fold_results.append(res)
+    precision.append(round(metrics.precision_score(test_y, predicted, average="micro"), 2))
+    recall.append(round(metrics.recall_score(test_y, predicted, average="micro"), 2))
+    f1.append(round(metrics.f1_score(test_y, predicted, average="micro"), 2))
 
-# dataset loader
-data_loader = torch.utils.data.DataLoader(dataset=cells_test, batch_size=batch_size, shuffle=True, drop_last=True)
+print("Precision:", np.average(np.array(precision)), "Recall:", np.average(np.array(recall)), "F1-score:",
+      np.average(np.array(f1)))
 
-test_y = []
-predicted = []
-
-import numpy as np
-
-for X, Y in data_loader:
-    X = X.to(device)
-    Y = Y.to(device)
-    print(len(X), len(Y))
-    optimizer.zero_grad()
-    hypothesis = model(X)
-    cost = criterion(hypothesis, Y)
-    y_true = Y.cpu().detach().numpy()
-
-    for i in range(batch_size):
-        test_y.append(y_true[i])
-        if np.array(hypothesis[i].cpu().detach().numpy())[Y[i].cpu().detach().numpy()] == np.max(
-            np.array(hypothesis[i].cpu().detach().numpy())):
-            predicted.append(y_true[i])
-        else:
-            for k in range(len(np.array(hypothesis[i].cpu().detach().numpy()))):
-                tmp = np.array(hypothesis[i].cpu().detach().numpy())
-                if tmp[k] == np.max(tmp):
-                    predicted.append(k)
-                    break
-
-    # cost.backward()
-    # optimizer.step()
-
-test_y = np.array(test_y)
-predicted = np.array(predicted)
-print(test_y)
-print(predicted)
-
-print("Precision:", round(metrics.precision_score(test_y, predicted, average="micro"), 3),
-      "Recall:", round(metrics.recall_score(test_y, predicted, average="micro"), 3),
-      "F1-score:", round(metrics.f1_score(test_y, predicted, average="micro"), 3))
+for res in fold_results:
+    print(res)
+    f = open("auc_fold.txt", "a")
+    f.write(res)
+    f.close()
